26e4f9696a27c92de6c944622c2aaaee5463ab46
Sapna7141 <104247206+Sapna7141@users.noreply.github.com>
2022-05-19 16:26:39 +0530

    Create creating_webserver_in_STA_mode.ino

---
+++ WebServer/creating_webserver_in_STA_mode.ino
@@ -0,0 +1,56 @@
+// Creating a Web Server in STA MODE
+
+
+#include <WiFi.h>
+#include <WebServer.h>
+
+//// SSID & Password
+const char* ssid = "redmi";  // Enter your SSID here
+const char* password = "sapnasharma";  //Enter your Password here
+
+
+WebServer server(80);  // Object of WebServer(HTTP port, 80 is default)
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Try Connecting to ");
+  Serial.println(ssid);
+
+
+  // Connect to your wi-fi modem
+  WiFi.begin(ssid, password);
+
+  // Check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected successfully");
+  Serial.print("Got IP: ");
+  Serial.println(WiFi.localIP());  //Show ESP32 IP on serial
+
+  server.on("/", handle_root);
+
+  server.begin();// to start server
+  Serial.println("HTTP server started");
+  delay(100); 
+}
+void loop() {
+  server.handleClient();
+}
+
+// HTML contents which display on web server
+String HTML = "<!DOCTYPE html>\
+<html>\
+<body>\
+<h1>My First Web Server with ESP32 - Station Mode</h1>\
+</body>\
+</html>";
+
+// Handle root url 
+void handle_root() {
+  server.send(200, "text/html", HTML);
+}
+
